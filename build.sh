19b88fd6022f2ef165e1e55c58b86ce30894a58d
Stuart Taylor <229520+stuartraetaylor@users.noreply.github.com>
2026-05-29 17:12:15 +0100

    Remove comment from build.sh

--- build.sh
+++ build.sh
@@ -10,5 +10,3 @@ find ${BIN} -type f -delete
 ${CC} ${CFLAGS} holysnake.c -o ${BIN}/holysnake -lSDL2
 ${CC} ${CFLAGS} mapwalk.c -o ${BIN}/mapwalk -lSDL2
 ${CC} ${CFLAGS} mapwalk2.c -o ${BIN}/mapwalk2 -lSDL2
-
-# mapwalk3d is multi-file and self-contained: see mapwalk3d/Makefile
